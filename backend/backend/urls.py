944b18c0e2c414e83f2cd0a00914f22989eb10ab
keane <keane7a@gmail.com>
2025-10-17 15:02:19 +0100

    add apis

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from users.urls import router as users_router
+
+router = routers.DefaultRouter()
+
+router.registry.extend(users_router.registry)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
