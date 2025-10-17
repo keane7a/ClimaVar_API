74c4e06dd9f5b01bed146e4424439b9e776a1328
keane <keane7a@gmail.com>
2025-10-17 13:42:13 +0100

    add password to user serializer

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
     
     class Meta:
         model = User
