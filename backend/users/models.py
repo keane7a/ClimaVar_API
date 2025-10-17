59b3d521f8eabde882213dce1829e0ca587a18a4
keane <keane7a@gmail.com>
2025-10-17 15:02:30 +0100

    fix user create error

--- backend/users/models.py
+++ backend/users/models.py
@@ -9,3 +9,4 @@ class User(AbstractBaseUser):
     email = models.EmailField(max_length=255, unique=True)
     first_name = models.CharField(max_length=255, blank=True, null=True)
     last_name = models.CharField(max_length=255, blank=True, null=True)
+    USERNAME_FIELD = 'username'
