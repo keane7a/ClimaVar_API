207a0284dc9dfdec3cf2a3a9e1aa7d7bd5cc90b3
keane <keane7a@gmail.com>
2025-10-17 16:22:39 +0100

    init misclassification API endpoint

---
+++ backend/misclassification/urls.py
@@ -0,0 +1,6 @@
+from rest_framework.routers import SimpleRouter
+from misclassification.views import MisclassificationViewSet
+
+
+router = SimpleRouter()
+router.register(r"misclassifications", MisclassificationViewSet, basename="misclassifications")
