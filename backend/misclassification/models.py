d280454cdee8e8f6877833bfb38cb7ab48e79cfc
keane <keane7a@gmail.com>
2025-10-17 16:22:39 +0100

    init misclassification API endpoint

--- backend/misclassification/models.py
+++ backend/misclassification/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class MisclassificationLog(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user_input = models.TextField(max_length=300)
+    llm_output = models.TextField(max_length=300)
+    timestamp = models.DateTimeField(auto_now_add=True)
--- backend/misclassification/views.py
+++ backend/misclassification/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
+from misclassification.models import MisclassificationLog
+from misclassification.serializer import MisclassificationLogSerializer
 # Create your views here.
+
+class MisclassificationViewSet(viewsets.ModelViewSet):
+    queryset = MisclassificationLog.objects.all()
+    serializer_class = MisclassificationLogSerializer
+    permission_classes = [IsAdminUser]
+    
+    
+    @action(detail=True, methods=['post'], permission_classes=[AllowAny], url_path='check-misclassification')
+    def check_misclassification(self, request):
+        
+        return Response({"status": "Misclassification endpoint is working"}, status=status.HTTP_200_OK)
