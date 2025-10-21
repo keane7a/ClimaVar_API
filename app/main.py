5e0ce28af8999566d427faeea77655f6f4000819
keane <keane7a@gmail.com>
2025-10-21 15:10:27 +0100

    run black

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI, APIRouter, HTTPException
 from pydantic import BaseModel
-from utils import validate_input, cards_classify_claim, build_prompt, llm_answer, extract_final
+from utils import (
+    validate_input,
+    cards_classify_claim,
+    build_prompt,
+    llm_answer,
+    extract_final,
+)
 from dotenv import load_dotenv
 import os
 
@@ -8,17 +14,20 @@ import os
 load_dotenv()
 CLIMAVAR_TOKEN = os.getenv("CLIMAVAR_TOKEN")
 
+
 class Item(BaseModel):
     text: str
     token: str
 
+
 app = FastAPI()
 router = APIRouter()
+
 
 @router.post("/check-misinformation/", status_code=200)
 def check_misinformation(item: Item):
     """
-        Evaluate a user-supplied climate claim and return a concise fact-check.
+    Evaluate a user-supplied climate claim and return a concise fact-check.
     """
     """
     End-to-end:
@@ -29,19 +38,19 @@ def check_misinformation(item: Item):
     5) Extract FINAL (≤300 chars)
     6) Log with columns indicating prompt family and chain-of-thought
     """
-    
+
     # Init parameters
-    prompt_version = "i0"   # choose among: i0,i1,i2,s0,s1,s2
+    prompt_version = "i0"  # choose among: i0,i1,i2,s0,s1,s2
     model = "gpt-4o-mini"
     temperature = 0.2
-    
+
     text = item.text
     token = item.token
-    
+
     # THIS IS BAD DESIGN DO NOT FOLLOW!! THIS IS NON-CRITICAL AND ONLY TO BLOCK BASIC ACCESS
     if token != CLIMAVAR_TOKEN:
         return HTTPException(status_code=401, detail="Invalid token")
-    
+
     # 1) Validate
     err = validate_input(text)
     if err:
@@ -68,7 +77,8 @@ def check_misinformation(item: Item):
     #     uses_cot=uses_cot,
     #     final_text=final
     # )
-    
+
     return {"response": extract_final(raw), "misinformation": cards.is_misinformation}
+
 
 app.include_router(router)
