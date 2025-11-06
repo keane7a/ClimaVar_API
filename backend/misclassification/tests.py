6c68dc83fe88c3a57b9b9fd8e098df837f3161ce
keane <keane7a@gmail.com>
2025-11-06 15:54:12 +0000

    run black

--- backend/misclassification/tests.py
+++ backend/misclassification/tests.py
@@ -6,57 +6,51 @@ from rest_framework.authtoken.models import Token
 
 class MisclassificationTest(APITestCase):
     def setUp(self):
-        self.check_misclassification_url = "/api/misclassifications/check-misclassification/"
+        self.check_misclassification_url = (
+            "/api/misclassifications/check-misclassification/"
+        )
         self.user = User.objects.create_user(
-            username="testuser",
-            email="admin@example.com",
-            password="password"
+            username="testuser", email="admin@example.com", password="password"
         )
         self.user_token = Token.objects.get_or_create(user=self.user)[0].key
-    
-    
+
     def test_post_check_misclassification(self):
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user_token)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token)
         response = self.client.post(
             self.check_misclassification_url,
-            data={
-                "text": "Climate change is a religion."
-            }
+            data={"text": "Climate change is a religion."},
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        #self.assertEqual(1, response.data["misinformation"]) # True it is a misinformation
+        # self.assertEqual(1, response.data["misinformation"]) # True it is a misinformation
         self.assertIn("llm_response", response.data)
-        
+
     def test_post_check_misclassification_character_range(self):
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user_token)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token)
         response = self.client.post(
-            self.check_misclassification_url,
-            data={
-                "text": "Climate"
-            }
+            self.check_misclassification_url, data={"text": "Climate"}
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("Input", response.data["message"])
-    
+
     def test_post_check_misclassification_unsupported_traslation(self):
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user_token)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token)
         response = self.client.post(
             self.check_misclassification_url,
             data={
                 "text": "Halo, anda siapa ya?",
-            }
+            },
         )
-        
+
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("Unsupported", response.data["message"])
 
     def test_post_check_misclassification_not_climate_related(self):
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.user_token)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token)
         response = self.client.post(
             self.check_misclassification_url,
             data={
                 "text": "This is a house.",
-            }
+            },
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("Query", response.data["message"])
--- backend/misclassification/views.py
+++ backend/misclassification/views.py
@@ -103,7 +103,7 @@ class MisclassificationViewSet(viewsets.ModelViewSet):
             if chunk_id:
                 chunk_id = chunk_id.split("_")[0]
             cites.append(f"{title}, page number {chunk_id} ({year}) - {url}")
-        
+
         return "".join(lines), list(set(cites))
 
     @extend_schema(
@@ -241,8 +241,10 @@ class MisclassificationViewSet(viewsets.ModelViewSet):
         )
         print(cites, "here")
         return Response(
-            {"llm_response": final_answer, 
-             "misinformation": int(is_misinformation), 
-             "references": cites},
+            {
+                "llm_response": final_answer,
+                "misinformation": int(is_misinformation),
+                "references": cites,
+            },
             status=status.HTTP_200_OK,
         )
