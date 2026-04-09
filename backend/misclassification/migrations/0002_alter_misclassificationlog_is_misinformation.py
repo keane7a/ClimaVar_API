e6b494a874a82ca6b72d579ffafcac7ee6c7d60a
Marcos Oliveira Jr. <marcosjr_06@hotmail.com>
2026-04-09 11:33:01 -0300

    Create 0002_alter_misclassificationlog_is_misinformation.py
    Migration: change is_misinformation from boolean to integer (0=accurate, 1=misinfo, 2=partial)

---
+++ backend/misclassification/migrations/0002_alter_misclassificationlog_is_misinformation.py
@@ -0,0 +1,42 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('misclassification', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql="""
+                ALTER TABLE misclassification_misclassificationlog 
+                ALTER COLUMN is_misinformation TYPE integer 
+                USING CASE 
+                    WHEN is_misinformation = true THEN 1 
+                    WHEN is_misinformation = false THEN 0 
+                    ELSE 0 
+                END;
+            """,
+            reverse_sql="""
+                ALTER TABLE misclassification_misclassificationlog 
+                ALTER COLUMN is_misinformation TYPE boolean 
+                USING CASE 
+                    WHEN is_misinformation = 1 THEN true 
+                    ELSE false 
+                END;
+            """,
+        ),
+        migrations.AlterField(
+            model_name='misclassificationlog',
+            name='is_misinformation',
+            field=models.IntegerField(
+                default=0,
+                choices=[
+                    (0, 'Accurate'),
+                    (1, 'Misinformation'),
+                    (2, 'Partial / Needs context'),
+                ],
+            ),
+        ),
+    ]
